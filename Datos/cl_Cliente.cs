f21bc76b1faa1d4540c26f1c734e92b7e6f1c675
AngelValdiviezo1994Gmail <131912926+AngelValdiviezo1994Gmail@users.noreply.github.com>
2026-03-25 18:03:21 -0500

    Creación de proyecto web

---
+++ Datos/cl_Cliente.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Datos
+{
+    public class cl_Cliente
+    {
+        public int Id { get; set; }
+        
+        public string Nombres { get; set; }
+        
+        public string Apellidos { get; set; }
+        
+        public string RazonSocial { get; set; }
+
+        public DateTime Fecha_Nacimiento { get; set; }
+
+        public int IdTipoIdentificacion { get; set; }
+
+        public string NumIdentificacion { get; set; }
+        
+        public string Telefono { get; set; }
+        
+        public string Direccion { get; set; }
+        
+        public string Email { get; set; }
+
+        public string EstadoCivil { get; set; }
+        
+        public string TendenciaCompra { get; set; }
+        
+        public string UsuarioCreacion { get; set; }
+
+        public DateTime Fecha_Creacion { get; set; }
+
+        public string UsuarioModificacion { get; set; }
+
+        public DateTime? Fecha_Modificacion { get; set; }
+    }
+}
---
+++ Datos/cl_Producto.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Datos
+{
+    public class cl_Producto
+    {
+        public int Id { get; set; }
+        
+		public int IdTipoProducto { get; set; }
+
+        public string Codigo { get; set; }
+        
+		public string Nombre { get; set; }
+
+		public string PrecioUnitario { get; set; }
+		
+		public string Estado { get; set; }
+		
+		public int Stock { get; set; }
+		
+		public string UsuarioCreacion { get; set; }
+		
+		public string UsuarioModificacion { get; set; }
+    }
+}
